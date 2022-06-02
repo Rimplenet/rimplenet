f6ce7996cd06a0dfb4133d5c9e0a1484dcd62b86
Arrikk <horpeyhermi@gmail.com>
2022-06-02 18:04:55 +0100

    Users Fe

---
+++ admin/users/create-users.php
@@ -0,0 +1,32 @@
+<?php
+
+class RimplenetAdminCreateUsers
+{
+
+    public function __construct()
+    {
+       add_action( 'admin_menu', array( $this, 'rimplenet_admin_menu_create_Users' ) );
+    }
+
+   public function rimplenet_admin_menu_create_Users()
+   {
+     add_submenu_page(
+            'edit.php?post_type=rimplenettransaction',
+            __( 'Create Users', 'rimplenet' ),
+            __( 'Create Users', 'rimplenet' ),
+            'manage_options',
+            'create_Users',
+            array( $this, 'create_Users_fxn' )
+            );
+  }
+
+  public function create_Users_fxn(){
+ 
+      include_once plugin_dir_path( dirname( __FILE__ ) ) . '/layouts/admin-settings-users.php';
+
+    }
+   
+
+}
+
+$RimplenetAdminCreateUsers = new RimplenetAdminCreateUsers();
