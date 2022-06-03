ccc06f2452fbe1e529e5c74479c88c9101cd2379
tjava <mustyytee@gmail.com>
2022-06-03 16:34:24 +0100

    referrals ui

--- admin/referrals/class-tab-manager.php
+++ admin/referrals/class-tab-manager.php
@@ -5,25 +5,25 @@ class RimplenetAdminClassTabManagerReferrals
 
     public function __construct()
     {
-       add_action( 'admin_menu', array( $this, 'rimplenet_admin_menu_create_referrals' ) );
+        add_action( 'admin_menu', array( $this, 'rimplenet_admin_menu_create_referrals' ) );
     }
 
-   public function rimplenet_admin_menu_create_referrals()
-   {
-    add_submenu_page(
-        'edit.php?post_type=rimplenettransaction',
-        __( 'Create referrals Tab', 'rimplenet' ),
-        __( 'Create referrals Tab', 'rimplenet' ),
-        'manage_options',
-        'create_referrals_tab',
-        array( $this, 'create_referrals_fxn' )
-    );
-  }
+    public function rimplenet_admin_menu_create_referrals()
+    {
+        add_submenu_page(
+            'edit.php?post_type=rimplenettransaction',
+            __( 'Referrals', 'rimplenet' ),
+            __( 'Referrals', 'rimplenet' ),
+            'manage_options',
+            'referrals_tab',
+            array( $this, 'create_referrals_fxn' )
+        );
+    }
 
-  public function create_referrals_fxn(){
+    public function create_referrals_fxn(){
  
-      include_once plugin_dir_path( dirname( __FILE__ ) ) . '/referrals/layouts/tab-manager.php';
-      // echo "hello";
+        include_once plugin_dir_path( dirname( __FILE__ ) ) . '/referrals/layouts/tab-manager.php';
+        // echo "hello";
 
     }
    
