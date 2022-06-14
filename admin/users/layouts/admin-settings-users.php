a1f148498feec53c77746a5ab3c0adb564919afa
Arrikk <horpeyhermi@gmail.com>
2022-06-14 02:14:46 +0100

    Completed Update Users

--- admin/users/layouts/admin-settings-users.php
+++ admin/users/layouts/admin-settings-users.php
@@ -17,25 +17,25 @@ require plugin_dir_path(dirname(__FILE__)) . '/assets/php/update.php';
                 <label for="fname">First Name
                 <span class="dashicons dashicons-editor-help rimplenet-admin-tooltip" title="First Name (Your birth name)"></span>
                 </label>
-                <input type="text" name="fname" id="fname" class="form-input" placeholder="First Name" value="<?= $user->first_name ?: '' ?>">
+                <input type="text" name="fname" id="fname" class="form-input" placeholder="First Name" value="<?= $user['first_name'] ?: '' ?>">
             </div>
             <div class="control">
                 <label for="lname">Last Name
                 <span class="dashicons dashicons-editor-help rimplenet-admin-tooltip" title="Last Name (Your Family name)"></span>
                 </label>
-                <input type="text" name="lname" id="lname" class="form-input" placeholder="Last Name" value="<?= $user->last_name ?: '' ?>">
+                <input type="text" name="lname" id="lname" class="form-input" placeholder="Last Name" value="<?= $user['last_name'] ?: '' ?>">
             </div>
             <div class="control">
                 <label for="lname">Username
                 <span class="dashicons dashicons-editor-help rimplenet-admin-tooltip" title="Username (Choose a preferred name u would like)"></span>
                 </label>
-                <input type="text" name="uname" id="uname" class="form-input" placeholder="Username" value="<?= $user->display_name ?: '' ?>">
+                <input type="text" name="uname" id="uname" class="form-input" placeholder="Username" value="<?= $user['display_name'] ?: '' ?>">
             </div>
             <div class="control">
                 <label for="email">Email
                 <span class="dashicons dashicons-editor-help rimplenet-admin-tooltip" title="Email Address, Please enter a valid email address"></span>
                 </label>
-                <input type="email" name="email" id="email" class="form-input" placeholder="Email" value="<?= $user->user_email ?: '' ?>">
+                <input type="email" name="email" id="email" class="form-input" placeholder="Email" value="<?= $user['user_email'] ?: '' ?>">
             </div>
             <?php if(!isset($user)): ?>
             <div class="control">
@@ -52,7 +52,7 @@ require plugin_dir_path(dirname(__FILE__)) . '/assets/php/update.php';
             </div>
             <?php endif; ?>
             <div class="control">
-                <input type="hidden" name="user_id" value="<?= $user->ID ?: '' ?>">
+                <input type="hidden" name="user_id" value="<?= $user['ID'] ?: '' ?>">
                 <input type="submit" name="<?= $user ? 'update_user' : 'create_user' ?>" class="button button-primary submit-btn" value="<?= $user ? 'Update' : 'Register' ?>">
                     
             </div>
