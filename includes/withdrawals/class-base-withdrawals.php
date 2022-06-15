39cf2226cc75a5aa0ee59380c0dddc4c58a0d675
Arrikk <horpeyhermi@gmail.com>
2022-06-15 17:11:21 +0100

    Completed Transfer

--- includes/withdrawals/class-base-withdrawals.php
+++ includes/withdrawals/class-base-withdrawals.php
@@ -9,11 +9,8 @@ class Rimplenet_Base_Withdrawals {
 
     private function load_required_files() {
    	 //Add Required Files to Load
-	//  require_once plugin_dir_path( dirname( __FILE__ ) ) . 'wallets/base-wallet.php';
+	 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'withdrawals/base-withdrawal.php';
 	 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'withdrawals/create-withdrawals.php';
-	//  require_once plugin_dir_path( dirname( __FILE__ ) ) . 'wallets/get-wallets.php';
-	//  require_once plugin_dir_path( dirname( __FILE__ ) ) . 'wallets/update-wallets.php';
-	//  require_once plugin_dir_path( dirname( __FILE__ ) ) . 'wallets/delete-wallets.php';
     }
 	
 }
