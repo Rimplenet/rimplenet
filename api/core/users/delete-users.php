c9b75c967698db2a3c165a245c4e199cfb3f2e46
tjava <mustyytee@gmail.com>
2022-06-18 16:15:14 +0100

    fix regex

--- api/core/users/delete-users.php
+++ api/core/users/delete-users.php
@@ -13,7 +13,7 @@ class RimplenetDeleteUserApi
     public function register_api_routes()
     {
         register_rest_route(
-            'rimplenet/v1', '/users',
+            'rimplenet/v1', '/users/(?P<user_id>\d+)',
             [
                 'methods' => 'DELETE',
                 'callback' => [$this, 'delete_user']
@@ -23,11 +23,15 @@ class RimplenetDeleteUserApi
 
     public function delete_user(WP_REST_Request $request)
     {
+        $user_id = sanitize_text_field($request->get_param('user_id'));
+
+        $headers = getallheaders();
+        $access_token = $headers['Authorization'];
 
         $user = new RimplenetDeleteUser();
         $delete_user = $user->delete_user(
-            $request->get_param('caller_id'),
-            $request->get_param('user_id')
+            $user_id,
+            $access_token
         );
         
         return new WP_REST_Response($delete_user);
