ec3bc740e5ba6b0b44231c22eca89f39468a9905
DanielMabadeje <mabadejedaniel1@gmail.com>
2022-06-24 10:51:45 +0100

    added folder structure

---
+++ includes/email-trait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Traits\Wallet;
+
+trait RimplenetEmailTrait
+{
+  public function sendWelcomeMail($email)
+  {
+    # code...
+  }
+
+  public function sendRestPasswordMail(Type $var = null)
+  {
+    # code...
+  }
+}
