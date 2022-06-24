2612135599d1a5aab3b69deb79120f38f3c6ca7f
DanielMabadeje <mabadejedaniel1@gmail.com>
2022-06-24 22:51:56 +0100

    Added investment package

---
+++ api/core/investment-packages/class-base-investment-packages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Rimplenet_Base_Investment_Package_Api
+{
+
+	private $plugin_name;
+
+	private $version;
+
+	public function __construct()
+	{
+		$this->plugin_name = $plugin_name ?? 'RimplenetIn';
+		$this->version = $version ?? 'v1';
+		$this->load_required_files();
+	}
+	private function load_required_files()
+	{
+		//Add Required Files to Load
+		
+	}
+}
+
+
+$Rimplenet_Base_Investment_Package_Api = new Rimplenet_Base_Investment_Package_Api();
