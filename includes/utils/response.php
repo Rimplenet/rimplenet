663b4d22614949547286fdf2b37180cbffcf6c36
Arrikk <horpeyhermi@gmail.com>
2022-09-17 20:40:11 +0100

    Update response.php

--- includes/utils/response.php
+++ includes/utils/response.php
@@ -9,8 +9,8 @@ class Res
             'status_code' => $status,
             'status' => false,
             'message' => $message,
+            'error' => Utils::$response['error'] ?? $err,
             'submitted' => self::$submitted,
-            'error' => Utils::$response['error'] ?? $err
         ];
 
         if(count(self::$submitted) == 0) unset(Utils::$response['submitted']);
@@ -25,12 +25,12 @@ class Res
             'status_code' => $status,
             'status' => true,
             'message' => $message,
+            'data' => $data,
             'submitted' => self::$submitted,
-            'data' => $data
         ];
         
         if(count(self::$submitted) == 0) unset(Utils::$response['submitted']);
-        
+
         status_header(Utils::$response['status_code']);
         return true;
     }
