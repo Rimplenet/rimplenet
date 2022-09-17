9b7ab3ce48e889a51173c8dc02372c468d559bbd
georgeessien <113784209+georgeessien@users.noreply.github.com>
2022-09-17 19:43:11 +0100

    Added stuff

--- includes/user-wallet-balance/user-wallet-balance.php
+++ includes/user-wallet-balance/user-wallet-balance.php
@@ -19,22 +19,25 @@ class RimplenetGetWalletBalance extends RimplenetBalance
         foreach ($wallet_id as $key => $value) {
             // $this->wallet_id = $value;
             if ($this->getWalletById($value)) {
-                $data[$value]=$this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value);
-                if ($formatted=="yes") {
-                    $data['formatted'][$value]= $this->getRimplenetWalletFormattedAmount($this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value), $value);
-                }
+                $data[$value]["raw"]=$this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value);
+                // if ($formatted=="yes") {
+                    // $data[$key][$value]['formatted']= $this->getRimplenetWalletFormattedAmount($this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value), $value);
+                    $data[$value]['formatted']= $this->getRimplenetWalletFormattedAmount($this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value), $value);
+                // }
             }else{
                 continue;
             }
                 // return Res::error($this->error, "Wallet Already Exists", 409);
             // $data[$value]=$this->get_withdrawable_wallet_bal($user_id, $value) + $this->get_nonwithdrawable_wallet_bal($user_id, $value);
         }
+        // $data['submitted']='';
 
         return self::$response = [
                 'status_code' => 200,
                 'status' => true,
                 'message' => 'Wallet Balance retrieved',
-                'data' => $data
+                'data' => $data,
+                'submitted'=>$wallet_id
             ];
 
         // $balance = $this->get_withdrawable_wallet_bal($user_id, $wallet_id) + $this->get_nonwithdrawable_wallet_bal($user_id, $wallet_id);
