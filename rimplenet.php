8e377856a4917b99b339d2af36f066c78c3bd675
DanielMabadeje <mabadejedaniel1@gmail.com>
2022-09-30 15:23:36 +0100

    Added new hook

--- rimplenet.php
+++ rimplenet.php
@@ -77,7 +77,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-rimplenet.php';
  Class Rimplenet_Launcher{
 	public function __construct()
 	{
-		add_action('init', array($this, 'run_rimplenet'), 10);
+		add_action('wp', array($this, 'run_rimplenet'), 10);
 		
 	}
 
