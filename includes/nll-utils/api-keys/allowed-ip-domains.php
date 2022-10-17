fc43a5c7bd2e340a733f5f28a2e66e20f9b088c6
Arrikk <horpeyhermi@gmail.com>
2022-10-17 17:18:55 +0100

    Updates.. API Keys

--- includes/nll-utils/api-keys/allowed-ip-domains.php
+++ includes/nll-utils/api-keys/allowed-ip-domains.php
@@ -2,31 +2,11 @@
 
 class AllowedIPAndDomains
 {
-    public static function ip()
-    {
-        $ips = trim(str_replace(['\r\n', ' ', PHP_EOL], '', get_option('allowed_ip_address')));
-
-        # if ip is not set ignore and return true
-        if ($ips === null || empty($ips)) return true;
-
-        // $ip_array = explode(',', $ips);
-        // // foreach($ip_array)
-    }
-    public static function domains()
+    public static function ip_domains()
     {
         $referral = self::host();
 
-        if (!filter_var($referral, FILTER_VALIDATE_URL)) :
-            Res::error([
-                'referral' => "invalid referral domain",
-                'domain' => $referral,
-                'process' => 'start'
-            ], 'Invalid referral domain', 403);
-            echo json_encode(Utils::$response);
-            exit;
-        endif;
-
-        $domains = trim(str_replace(['\r\n', ' ', PHP_EOL], '', get_option('allowed_domains')));
+        $domains = trim(str_replace(['\r\n', ' ', PHP_EOL], '', get_option('allowed_ip_domain')));
 
         # if domain is not set ignore and return true
         if ($domains === null || empty($domains)) return true;
@@ -36,7 +16,7 @@ class AllowedIPAndDomains
             if (empty($domain)) unset($domain_array[$key]);
         endforeach;
 
-        if (in_array($referral, $domain_array)) return true;
+        if (in_array($referral['ip'], $domain_array) || in_array($referral['host'], $domain_array)) return true;
 
         Res::error([
             'referral' => "invalid referral domain",
@@ -49,9 +29,10 @@ class AllowedIPAndDomains
 
     public static function host()
     {
-        $schema = $_SERVER['REQUEST_SCHEME'] ?? '';
-        $host = $_SERVER['SERVER_NAME'] ?? '';
 
-        return $schema.'://'.$host;
+        return [
+            'ip' => $_SERVER['REMOTE_ADDR'] ?? '',
+            'host' => $_SERVER['SERVER_NAME'] ?? ''
+        ];
     }
 }
