8fc60821d98210d101245134375824d8f723f044
DanielMabadeje <mabadejedaniel1@gmail.com>
2022-11-15 19:09:31 +0100

    edited email content for create debit

--- includes/emails/email-templates/create-debit.php
+++ includes/emails/email-templates/create-debit.php
@@ -1,5 +1,5 @@
 <?php
 
     $subject = 'Debit Alert';
-    $message = 'Your account has been debited with the sum of '.$data['amount'].' on transaction '.$data['transaction_id'].' with description: "'.$transaction['note'].'".  Please visit Middey for more enquiries and If this is not you, do not hesitate to change your password and contact us immediately';
+    $message = 'Your account has been debited with the sum of '.$data["amount"].' on transaction '.$data["transaction_id"].' with description: "'.$data["note"].'".  Please visit Middey for more enquiries and If this is not you, do not hesitate to change your password and contact us immediately';
     $headers = 'just a test';
