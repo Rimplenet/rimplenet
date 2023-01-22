6449f635bd6cc64ea9cfbc72b1a3d516d6eb109b
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-01-22 21:42:37 +0100

    update

--- api/core/credits/create-credits.php
+++ api/core/credits/create-credits.php
@@ -19,7 +19,7 @@ $createCredits = new class extends RimplenetCreateCredits
 
     public function api_create_credits(WP_REST_Request $req)
     {
-        do_action('rimplenet_api_request_started', $req, $allowed_roles = ['administrator'], $action = 'rimplenet_create_credits');
+        // do_action('rimplenet_api_request_started', $req, $allowed_roles = ['administrator'], $action = 'rimplenet_create_credits');
 
         $this->req = [
             'note'          => sanitize_text_field($req['note'] ?? ''),
