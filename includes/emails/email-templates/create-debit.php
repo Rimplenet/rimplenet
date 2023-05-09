5090833ad2a9ca8b6770456dfb9881f60c5b02f2
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2023-05-09 22:15:17 +0100

    Setting up ci/cd

--- includes/emails/email-templates/create-debit.php
+++ includes/emails/email-templates/create-debit.php
@@ -1,23 +1,85 @@
 <?php
 
     $subject = 'Debit Alert - '.$data['symbol'].$data["amount_formatted"];
-    $message = "
-                <div style='text-align:center; padding: 9%; align-self:center;'>
+    // $message = "
+    //             <div style='text-align:center; padding: 9%; align-self:center;'>
                     
-                    <h2>Debit Alert - ".$data['symbol'].$data['amount_formatted']."</h2>
-                    Your account has been debited with the sum 
-                    of <strong>".$data['symbol'].$data['amount_formatted']."</strong>
-                    on transaction <strong>#".$data['transaction_id']."</strong> with 
-                    description: ".$data['note'].". 
-                    Your new balance is <strong>".$data['symbol'].number_format($data['balance_after'], $data['decimal'])."</strong>
-                    Please visit Middey for more enquiries and If this is not you,
-                    do not hesitate to change your password and contact us immediately. <br>
+    //                 <h2>Debit Alert - ".$data['symbol'].$data['amount_formatted']."</h2>
+    //                 Your account has been debited with the sum 
+    //                 of <strong>".$data['symbol'].$data['amount_formatted']."</strong>
+    //                 on transaction <strong>#".$data['transaction_id']."</strong> with 
+    //                 description: ".$data['note'].". 
+    //                 Your new balance is <strong>".$data['symbol'].number_format($data['balance_after'], $data['decimal'])."</strong>
+    //                 Please visit Middey for more enquiries and If this is not you,
+    //                 do not hesitate to change your password and contact us immediately. <br>
   
                     
   
-                    <p style='margin-top:3%'>Thank you for choosing us <strong>&copy; <script>document.write(new Date().getFullYear())</script></strong></p>
+    //                 <p style='margin-top:3%'>Thank you for choosing us <strong>&copy; <script>document.write(new Date().getFullYear())</script></strong></p>
     
-                </div>";
+    //             </div>";
+
+    $message = "
+    <style>
+    table,td {
+      border: 1px solid white;
+      border-collapse: collapse;
+      
+      
+    }
+    
+</style>
+<div style='background-color:#fff'>
+
+<table style='width:100%'>
+  <tr>
+    <th colspan='2' style='background: #ccc;padding: 15px;'>
+       $blogname_uppercase : A NEW TRANSACTION HAS OCCURED ON YOUR ACCOUNT ~ @{$username}
+    </th>
+  </tr>
+
+  <tr>
+    <td style='background-color:#ccc; padding: 20px; text-align: left;'><strong>TRANSACTION TYPE</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $tnx_type_uppercase </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ddd; padding: 20px; text-align: left;'><strong>TRANSACTION AMOUNT</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $amount_formatted </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ccc; padding: 20px; text-align: left;'><strong>TRANSACTION NOTE</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $note </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ddd; padding: 20px; text-align: left;'><strong>UNCLEARED BALANCE</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $user_non_wdr_bal_formatted </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ccc; padding: 20px; text-align: left;'><strong>CLEARED/ AVAILABLE BALANCE</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $user_wdr_bal_formatted </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ddd; padding: 20px; text-align: left;'><strong>TOTAL BALANCE</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $user_balance_total_formatted </td>
+  </tr>
+  
+  <tr>
+    <td style='background-color:#ccc; padding: 20px; text-align: left;'><strong>TRANSACTION DATE / TIME</strong></td>
+    <td style='background-color:#eee; padding: 20px; text-align: left;'> $date_time </td>
+  </tr>
+  
+</table>
+ <p style='background:#2d2f33; color:white; padding:25px; text-align:center;'>
+    This email was sent from <a href='$blogurl' style='color: #d28787;'> $blogname_uppercase </a>
+    <small>Powered by <em><a href='https://rimplenet.com'>Rimplenet</a></em></small>
+ </p>
+<div/>
+    "
 
                 // <div style='width:100%; margin-top:15px; margin-bottom:15px;'>
                 //     <a href='".get_site_url()."' style='padding: 200px; padding-top:15px; padding-bottom: 15px; background-color: black; color:white; margin-top: 20px;  border-radius: 5px; cursor:pointer;'>Go to Dashboard</a>
